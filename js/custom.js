3bc8a55aa190b6548ae0425b5723ed39c42312e7
Oscar Lafarga <otech47@gmail.com>
2013-10-16 12:58:47 -0700

    responsive autocomplete

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 $(document).ready( function() {
+<<<<<<< HEAD
 	$( function() {
 		var eventTags = [
 		"Beyond Wonderland 2013",
@@ -118,7 +119,57 @@ $(document).ready( function() {
 		$( "#artists" ).autocomplete({
 			source: artistTags
 		});
+=======
+	var getEventTags = function() {
+		var artistSelection = $("input[id='artists']").val();
+		var postdata = { artist:artistSelection };
+		jQuery.ajax({
+			type: "POST",
+			url: '../scripts/eventTags.php',
+			data: postdata,
+			success: function(data) 
+			{
+				var autocompleteData = JSON.parse(data);
+				$("#events").autocomplete({
+					source: autocompleteData
+				});
+				$( "#events-xs" ).autocomplete({
+					source: autocompleteData
+				});
+			}
+		});
+	};
+	getEventTags();
+	$("input[id='events']").blur(function(){
+		getEventTags();
 	});
+
+	var getArtistTags = function() {
+		var eventSelection = $("#artists").val();
+		var postdata = { event:eventSelection };
+		jQuery.ajax({
+			type: "POST",
+			url: '../scripts/artistTags.php',
+			data: postdata,
+			success: function(data) 
+			{
+				var autocompleteData = JSON.parse(data);
+				$("#artists").autocomplete({
+					source: autocompleteData
+				});
+				$( "#artists-xs" ).autocomplete({
+					source: autocompleteData
+				});
+
+			}
+		});
+	};
+	getArtistTags();
+	$("input[id='events']").blur(function(){
+		getArtistTags();
+>>>>>>> 354fa97e52de1110f5676caba9d9f5732a6fc530
+	});
+
 	$("button.stredm-panel-button").click(function(){
 		var eventSelection = $("input[id='events']").val();
 		var artistSelection = $("input[id='artists']").val();
