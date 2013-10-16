8cb3e000cc0ba0d5a4de883231834d3a68b13342
Oscar Lafarga <otech47@gmail.com>
2013-10-16 13:02:52 -0700

    Merge branch 'master' of https://github.com/otech47/STREDM
    Conflicts:
    	js/custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 $(document).ready( function() {
+<<<<<<< HEAD
 	$( function() {
 		var eventTags = [
 		"Beyond Wonderland 2013",
@@ -187,6 +188,32 @@ $(document).ready( function() {
 	$("input[id='events']").blur(function(){
 		getEventTags();
 	});
+=======
+	var getEventTags = function() {
+		var artistSelection = $("input[id='artists']").val();
+		var postdata = { artist:artistSelection };
+		jQuery.ajax({
+			type: "POST",
+			url: '../scripts/eventTags.php',
+			data: postdata,
+			success: function(data) 
+			{
+				var autocompleteData = JSON.parse(data);
+				$("#events").autocomplete({
+					source: autocompleteData
+				});
+				$( "#events-xs" ).autocomplete({
+					source: autocompleteData
+				});
+			}
+		});
+	};
+	getEventTags();
+	$("input[id='events']").blur(function(){
+		getEventTags();
+	});
+
+>>>>>>> 354fa97e52de1110f5676caba9d9f5732a6fc530
 	var getArtistTags = function() {
 		var eventSelection = $("#artists").val();
 		var postdata = { event:eventSelection };
